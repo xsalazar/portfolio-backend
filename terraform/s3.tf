5cac7bb8e2072d6ec117fc18ca748ef2bec26dbc
Xavier Salazar <x.salazar1729@gmail.com>
2023-01-21 16:33:51 -0800

    Update S3 bucket

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -2,14 +2,14 @@ resource "aws_s3_bucket" "instance" {
   bucket = "xsalazar-portfolio-data"
 }
 
-resource "aws_s3_bucket_notification" "instance" {
-  bucket = aws_s3_bucket.instance.bucket
+# resource "aws_s3_bucket_notification" "instance" {
+#   bucket = aws_s3_bucket.instance.bucket
 
-  lambda_function {
-    events              = ["s3:ObjectCreated:*"]
-    lambda_function_arn = aws_lambda_function.instance.arn
-  }
-}
+#   lambda_function {
+#     events              = ["s3:ObjectCreated:*"]
+#     lambda_function_arn = aws_lambda_function.instance.arn
+#   }
+# }
 
 resource "aws_s3_bucket_policy" "s3_bucket_policy" {
   bucket = aws_s3_bucket.instance.id
@@ -28,7 +28,7 @@ data "aws_iam_policy_document" "s3_bucket_policy_document" {
     resources = [aws_lambda_function.instance.arn]
 
     condition {
-      test     = "ArnEquals"
+      test     = "ArnLike"
       variable = "AWS:SourceArn"
       values   = [aws_s3_bucket.instance.arn]
     }

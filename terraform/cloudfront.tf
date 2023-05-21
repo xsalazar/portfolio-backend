dbbd97d46e1c8176f1d7fb125851af89dc999b88
Xavier Salazar <x.salazar1729@gmail.com>
2023-05-21 16:27:26 -0700

    Add `cache_policy_id`

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -13,6 +13,7 @@ resource "aws_cloudfront_distribution" "instance" {
     cached_methods         = ["GET", "HEAD"]
     target_origin_id       = local.s3_origin_id
     viewer_protocol_policy = "allow-all"
+    cache_policy_id        = "portfolio-cloudfront-cache-policy"
   }
 
 

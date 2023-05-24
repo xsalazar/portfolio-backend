b9bd416cb8bb135f1ce322d6cb3447426542b28e
Xavier Salazar <x.salazar1729@gmail.com>
2023-05-23 18:56:59 -0700

    Update S3 bucket policy

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -6,7 +6,24 @@ resource "aws_s3_bucket" "instance" {
   bucket = "xsalazar-portfolio-data"
 }
 
-resource "aws_s3_bucket_acl" "instance" {
+resource "aws_s3_bucket_policy" "instance" {
   bucket = aws_s3_bucket.instance.id
-  acl    = "public-read"
+  policy = data.aws_iam_policy_document.instance.json
+}
+
+data "aws_iam_policy_document" "instance" {
+  statement {
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    actions = [
+      "s3:GetObject",
+    ]
+
+    resources = [
+      "${aws_s3_bucket.instance.arn}/*",
+    ]
+  }
 }
